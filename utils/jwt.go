f6db3dcc9fd84ca80368dddeb5a367f78d23be0a
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-05-23 23:03:42 +0700

    Feat: Add JWT utils

---
+++ utils/jwt.go
@@ -0,0 +1,74 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+
+var jwtSecret = []byte("่jwt-secret-key")
+
+
+type JWTClaim struct {
+	UserID string `json:"user_id"` 
+	Email  string `json:"email"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(userID string, email string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	
+	claims := &JWTClaim{
+		UserID: userID,
+		Email:  email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", err
+	}
+	
+	return tokenString, nil
+}
+
+
+func ValidateToken(signedToken string) (*JWTClaim, error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JWTClaim{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("unexpected signing method")
+			}
+			return jwtSecret, nil
+		},
+	)
+	
+	if err != nil {
+		return nil, err
+	}
+	
+
+	claims, ok := token.Claims.(*JWTClaim)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+	
+	if claims.ExpiresAt < time.Now().Unix() {
+		return nil, errors.New("token expired")
+	}
+	
+	return claims, nil
+}
+
+func GetJWTSecret() []byte {
+	return jwtSecret
+}
