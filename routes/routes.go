d679abdd904f9cf376c2bdd69b6a1b5d7d0bda29
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-05-26 00:05:35 +0700

    Add: Add health check endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,26 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+
 	"golang-test/controllers"
 	"golang-test/middleware"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 	
 	router.Use(middleware.LoggerMiddleware())
+
+		router.GET("/api/healthcheck", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "healthy",
+			"service": "golang-api",
+			"time":    time.Now().Format(time.RFC3339),
+		})
+	})
 
 	public := router.Group("/api")
 	{
